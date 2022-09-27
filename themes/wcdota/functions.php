b8904ba3120e489d3d414fd873dd37c97ec169fd
Philip Carlsson <philipcarlsson@hotmail.com>
2022-09-27 10:58:34 +0200

    creating branch 12 and ourVision block

--- themes/wcdota/functions.php
+++ themes/wcdota/functions.php
@@ -1,9 +1,10 @@
 <?php
 
-add_action( 'after_setup_theme', 'woocommerce_support' );
-function woocommerce_support() {
-   add_theme_support( 'woocommerce' );
-}  
+add_action('after_setup_theme', 'woocommerce_support');
+function woocommerce_support()
+{
+  add_theme_support('woocommerce');
+}
 
 // THEME ENQUEUE --------------------------------------------------
 
@@ -43,10 +44,11 @@ add_theme_support('post-thumbnails');
 
 // NAV CLASS ----------------------------------------------------------
 
-add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
+add_filter('nav_menu_css_class', 'special_nav_class', 10, 2);
 
-function special_nav_class ($classes, $item) {
-  if (in_array('current-menu-item', $classes) ){
+function special_nav_class($classes, $item)
+{
+  if (in_array('current-menu-item', $classes)) {
     $classes[] = 'active ';
   }
   return $classes;
@@ -56,21 +58,32 @@ function special_nav_class ($classes, $item) {
 
 add_action('acf/init', 'my_acf_init_block_types');
 
-function my_acf_init_block_types() {
+function my_acf_init_block_types()
+{
 
-    // Check function exists.
-    if( function_exists('acf_register_block_type') ) {
+  // Check function exists.
+  if (function_exists('acf_register_block_type')) {
 
-        // register a testimonial block.
-        acf_register_block_type(array(
-            'name'              => 'dummyblock',
-            'title'             => __('dummyblock'),
-            'description'       => __('A custom dummyblock block.'),
-            'render_template'   => 'template-parts/blocks/dummyblock-block.php',
-            'category'          => 'formatting',
-            'icon'              => 'admin-comments',
-            'keywords'          => array("dummyblock"),
-        ));
+    // register a testimonial block.
+    acf_register_block_type(array(
+      'name'              => 'dummyblock',
+      'title'             => __('dummyblock'),
+      'description'       => __('A custom dummyblock block.'),
+      'render_template'   => 'template-parts/blocks/dummyblock-block.php',
+      'category'          => 'formatting',
+      'icon'              => 'admin-comments',
+      'keywords'          => array("dummyblock"),
+    ));
 
-    }
+    /* block for our vision FRONT PAGE */
+    acf_register_block_type(array(
+      'name'              => 'ourVision',
+      'title'             => __('ourVision'),
+      'description'       => __('A custom block for our visions.'),
+      'render_template'   => 'template-parts/blocks/our-vision-block.php',
+      'category'          => 'formatting',
+      'icon'              => 'admin-comments',
+      'keywords'          => array("ourVision"),
+    ));
+  }
 }
