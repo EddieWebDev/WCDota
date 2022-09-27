5bcf88728dcb856f6cf25575d4ce14480875ea7e
Philip Carlsson <philipcarlsson@hotmail.com>
2022-09-27 16:37:09 +0200

    changing variable name and more styling

--- themes/wcdota/template-parts/blocks/product-suggestion.php
+++ themes/wcdota/template-parts/blocks/product-suggestion.php
@@ -9,24 +9,21 @@ $args = array(
 ?>
 <div class="product-suggestion">
     <?php
-    $loop = new WP_Query($args);
-    while ($loop->have_posts()) : $loop->the_post();
+    $product_suggestion = new WP_Query($args);
+    while ($product_suggestion->have_posts()) : $product_suggestion->the_post();
         global $product;
     ?>
-        <div>
-            <?php if (has_post_thumbnail($loop->post->ID))
-                echo get_the_post_thumbnail($loop->post->ID, 'shop_catalog');
+        <div class="suggestion-img">
+            <?php if (has_post_thumbnail($product_suggestion->post->ID))
+                echo get_the_post_thumbnail($product_suggestion->post->ID);
             ?>
         </div>
 
-        <div>
-
-
+        <div class="suggestion-text">
             <h3>
                 <?php the_title(); ?>
             </h3>
             <a href="<?php the_permalink(); ?>" id="id-<?php the_id(); ?>">Read More</a>
-
         </div>
 
     <?php
