cc5314ef93c323e8e9701e3b4893414c272ff043
Philip Carlsson <philipcarlsson@hotmail.com>
2022-09-27 11:10:28 +0200

    creating initial html code using ACF block

---
+++ themes/wcdota/template-parts/blocks/our-vision-block.php
@@ -0,0 +1,13 @@
+<div class="our-vision">
+    <h3>
+        <?php the_field("title"); ?>
+    </h3>
+
+    <p>
+        <?php the_field("text"); ?>
+    </p>
+
+    <button>
+        <?php the_field("button_text"); ?>
+    </button>
+</div>
