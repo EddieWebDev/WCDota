467c4ff30f69d683e44633e2f0b150e6658115ed
EddieWebDev <eddandersson@gmail.com>
2022-09-29 13:23:21 +0200

    fixat bilder, title, description och link

--- themes/wcdota/template-parts/blocks/category-start-block.php
+++ themes/wcdota/template-parts/blocks/category-start-block.php
@@ -9,29 +9,29 @@ $categories = get_terms($args);
 
 
 
-<div>
-    <div>
+<div class="categories-container">
         <?php
         foreach($categories as $category){
             if("Uncategorized" !== $category->name){
-                //var_dump($category);
                 $term_link = get_term_link($category, "product_cat");
                 $term_id = $category->term_id;
                 $thumbnail_id = get_term_meta( $term_id, 'thumbnail_id', true );
-                var_dump($thumbnail_id);
                 $image = wp_get_attachment_url( $thumbnail_id );
-                var_dump($image);
-                echo "<img src='{$image}' alt='' width='762' height='365' />";
                 ?>
+                <div class="category">
                     <a href="<?php echo $term_link?>">
-                        <?php echo $category->name?>
-                    </a> 
-                <?php
-                
+                        <img src="<?php echo $image; ?>" alt="">
+                    </a>
 
+                    <a href="<?php echo $term_link?>">
+                        <h2><?php echo $category->name?></h2>
+                    </a>
+                    <p><?php echo $category->description;?></p>
+                    <a class="category-link" href="/wcdota/product-category/<?php echo $category->slug; ?>">Read More</a>
+                </div>
+                <?php
             }
         }
         ?>
-    </div>
 </div>
 
