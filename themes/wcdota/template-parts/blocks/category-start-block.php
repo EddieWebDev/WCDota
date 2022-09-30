ab7c3248bf8f8bacf195a2cd753c714c741709d1
Eddie Andersson <eddandersson@gmail.com>
2022-09-30 18:21:15 +0200

    fixing styling but not done

--- themes/wcdota/template-parts/blocks/category-start-block.php
+++ themes/wcdota/template-parts/blocks/category-start-block.php
@@ -7,31 +7,44 @@ $args = array(
 $categories = get_terms($args);
 ?>
 
+<div class="categories-wrapper">
 
+    <div class="categories-intro">
+        <h2>
+            <?php the_field("title"); ?>
+        </h2>
+        <p>
+            <?php the_field("content"); ?>
+        </p>
+    </div>  
 
-<div class="categories-container">
-        <?php
-        foreach($categories as $category){
-            if("Uncategorized" !== $category->name){
-                $term_link = get_term_link($category, "product_cat");
-                $term_id = $category->term_id;
-                $thumbnail_id = get_term_meta( $term_id, 'thumbnail_id', true );
-                $image = wp_get_attachment_url( $thumbnail_id );
-                ?>
-                <div class="category">
-                    <a href="<?php echo $term_link?>">
-                        <img src="<?php echo $image; ?>" alt="">
-                    </a>
-
-                    <a href="<?php echo $term_link?>">
-                        <h2><?php echo $category->name?></h2>
-                    </a>
-                    <p><?php echo $category->description;?></p>
-                    <a class="category-link" href="/wcdota/product-category/<?php echo $category->slug; ?>">Read More</a>
-                </div>
-                <?php
+    <div class="categories-container">  
+            <?php
+            foreach($categories as $category){
+                if("Uncategorized" !== $category->name){
+                    $term_link = get_term_link($category, "product_cat");
+                    $term_id = $category->term_id;
+                    $thumbnail_id = get_term_meta( $term_id, 'thumbnail_id', true );
+                    $image = wp_get_attachment_url( $thumbnail_id );
+                    ?>
+                    <div class="category">
+                        <div class="category-image">
+                            <a href="<?php echo $term_link?>">
+                                <img src="<?php echo $image; ?>" alt="">
+                            </a>
+                        </div>
+                        <div class="category-text">
+                            <a href="<?php echo $term_link?>">
+                                <h2><?php echo $category->name?></h2>
+                            </a>
+                            <p><?php echo $category->description;?></p>
+                            <a class="category-link" href="/wcdota/product-category/<?php echo $category->slug; ?>">Read More</a>
+                        </div>
+                    </div>
+                    <?php
+                }
             }
-        }
-        ?>
+            ?>
+    </div>
 </div>
 
