0aea2d126dec475c9b760ce0afb8ba9606b36fbc
Philip Carlsson <philipcarlsson@hotmail.com>
2022-10-03 12:51:53 +0200

    finishing styling of news block

--- themes/wcdota/template-parts/blocks/news-posts.php
+++ themes/wcdota/template-parts/blocks/news-posts.php
@@ -1,33 +1,36 @@
 <section class="news-section">
-    <!-- looping through custom post type POSTS -->
-    <div class="news-array">
-        <?php
-        $posts = get_field("number_of_posts");
+    <div>
+        <!-- looping through POSTS -->
+        <div class="news-array">
+            <?php
+            $posts = get_field("number_of_posts");
 
-        $args = array(
-            'post_type' => 'post',
-            'posts_per_page' => $posts
-        );
-        $the_query = new WP_Query($args); ?>
+            $args = array(
+                'post_type' => 'post',
+                'posts_per_page' => $posts
+            );
+            $the_query = new WP_Query($args); ?>
 
-        <?php if ($the_query->have_posts()) : ?>
+            <?php if ($the_query->have_posts()) : ?>
 
-            <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
-                <div class="news-container">
+                <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+                    <div class="news-container">
+                        <div class="img-div">
+                            <?php the_post_thumbnail("thumbnail"); ?>
+                        </div>
+                        <div class="post-div">
+                            <h3 class="news-name"><?php the_title(); ?></h3>
 
-                    <?php the_post_thumbnail("thumbnail"); ?>
+                            <?php the_excerpt(); ?>
 
-                    <h3 class="news-name"><?php the_title(); ?></h3>
+                            <a href="<?php the_permalink(); ?>">Read More</a>
+                        </div>
+                    </div>
+                <?php endwhile; ?>
 
-                    <?php the_excerpt(); ?>
+                <?php wp_reset_postdata(); ?>
 
-                    <a href="<?php the_permalink(); ?>">Read More</a>
-
-                </div>
-            <?php endwhile; ?>
-
-            <?php wp_reset_postdata(); ?>
-
-        <?php endif; ?>
+            <?php endif; ?>
+        </div>
     </div>
 </section>
