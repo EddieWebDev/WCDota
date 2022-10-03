9a62f54157e08b2157eaa15d96eb5d59320aa3f8
Philip Carlsson <philipcarlsson@hotmail.com>
2022-10-03 10:08:13 +0200

    updating json file

--- themes/wcdota/index.php
+++ themes/wcdota/index.php
@@ -1,6 +1,14 @@
 <?php get_header(); ?>
 
-
-
+<div class="posts">
+    <?php if (have_posts()) :
+        while (have_posts()) : the_post(); ?>
+            <div class="post-block">
+                <?php get_template_part("/template-parts/news-page"); ?>
+            </div>
+    <?php
+        endwhile;
+    endif; ?>
+</div>
 
 <?php get_footer(); ?>
