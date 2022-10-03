5b7da8e9869c38fc32d22769bf5e15c37a6f7544
Philip Carlsson <philipcarlsson@hotmail.com>
2022-10-03 13:28:36 +0200

    adding post thumbnail

--- themes/wcdota/single.php
+++ themes/wcdota/single.php
@@ -1,8 +1,8 @@
 <h1>SINGLE.PHP</h1>
-<!-- VI KANSKE INTE BEHÖVER HA SINGLE.PHP -->
 <?php get_header(); ?>
 
 <h2>MAIN</h2>
-<?php the_content(); ?>
+<?php the_post_thumbnail("medium"); ?>
+<?php the_content() ?>
 
 <?php get_footer(); ?>
