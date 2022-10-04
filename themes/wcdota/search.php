a98d28ddebdb213029afcf4e3f27576c9312ba7e
Philip Carlsson <philipcarlsson@hotmail.com>
2022-10-04 14:50:21 +0200

    completing styling for search.php

--- themes/wcdota/search.php
+++ themes/wcdota/search.php
@@ -2,31 +2,31 @@
 
 <header class="header">
 </header>
-
-<div class="search-div">
-    <h2 class="search-header">Sök</h2>
-    <span>Vad letar du efter?</span>
-    <?php get_search_form(); ?>
-</div>
-
-<?php if (have_posts()) : ?>
-    <p>Resultat</p>
-    <?php while (have_posts()) : the_post(); ?>
-
-        <div class="search-post">
-            <h3>
-                <?php the_title(); ?>
-            </h3>
-            <p>
-                <?php the_excerpt(); ?>
-            </p>
-            <a class="search-link" href="<?php the_permalink(); ?>">
-                VIEW COLLECTION
-            </a>
-        </div>
-<?php endwhile;
-else :
-    _e('Inga resultat hittades.', 'textdomain');
-endif; ?>
-
+<section>
+    <div class="search-div">
+        <h2 class="search-header">Sök</h2>
+        <span>Vad letar du efter?</span>
+        <?php get_search_form(); ?>
+    </div>
+    <div class="productsearch-results">
+        <h4 class="productsearch-header">Resultat</h4>
+        <?php if (have_posts()) : ?>
+            <?php while (have_posts()) : the_post(); ?>
+                <div class="search-post">
+                    <h3>
+                        <?php the_title(); ?>
+                    </h3>
+                    <p>
+                        <?php the_excerpt(); ?>
+                    </p>
+                    <a class="search-link" href="<?php the_permalink(); ?>">
+                        VIEW COLLECTION
+                    </a>
+                </div>
+            <?php endwhile; ?>
+        <?php else :
+            _e('Inga resultat hittades.', 'textdomain'); ?>
+        <?php endif; ?>
+    </div>
+</section>
 <?php get_footer();
