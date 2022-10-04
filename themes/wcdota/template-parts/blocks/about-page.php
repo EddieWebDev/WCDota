cd71ebd13c20f40d97ed3fd7af5fa0dbfcddca84
Philip Carlsson <philipcarlsson@hotmail.com>
2022-10-04 10:49:26 +0200

    styling about block

--- themes/wcdota/template-parts/blocks/about-page.php
+++ themes/wcdota/template-parts/blocks/about-page.php
@@ -5,16 +5,19 @@ $size = 'medium'; // (thumbnail, medium, large, full or custom size)
 ?>
 
 
-<div>
-    <h2>
+<div class="about-block">
+    <h2 class="about-header">
         <?php the_field("header"); ?>
     </h2>
 
-    <?php
-    if ($image) {
-        echo wp_get_attachment_image($image, $size);
-    } ?>
-    <p>
-        <?php the_field("text"); ?>
-    </p>
+    <div class="about-img">
+        <?php if ($image) {
+            echo wp_get_attachment_image($image, $size);
+        } ?>
+    </div>
+    <div class="about-content">
+        <p>
+            <?php the_field("text"); ?>
+        </p>
+    </div>
 </div>
