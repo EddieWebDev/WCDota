298dd239e279afb54ab6741dd1c7e6504a2c34c9
Philip Carlsson <philipcarlsson@hotmail.com>
2022-10-04 11:15:02 +0200

    styling contact text

--- themes/wcdota/template-parts/blocks/contact-text.php
+++ themes/wcdota/template-parts/blocks/contact-text.php
@@ -1,1 +1,12 @@
-<?php
+<div class="contact-block">
+    <div class="contact-header">
+        <h2>
+            <?php the_field("header"); ?>
+        </h2>
+    </div>
+    <div class="contact-content">
+        <p>
+            <?php the_field("text"); ?>
+        </p>
+    </div>
+</div>
