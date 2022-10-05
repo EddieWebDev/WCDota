7a89735d4d6520e175c8398edc47b122b237f100
Philip Carlsson <philipcarlsson@hotmail.com>
2022-10-05 22:03:05 +0200

    styling account page so its 100% width

--- themes/wcdota/woocommerce/myaccount/dashboard.php
+++ themes/wcdota/woocommerce/myaccount/dashboard.php
@@ -3,17 +3,12 @@
         <?php the_title(); ?>
     </h2>
 
-
     <!-- MY ORDERS.PHP    -->
-
     <?php wc_get_template_part("/template-parts/account-orders"); ?>
 
     <!-- MY ADDRESS.PHP -->
-
     <?php wc_get_template_part("/template-parts/account-address"); ?>
 
     <!-- FORM EDIT ACCOUNT.PHP -->
     <?php wc_get_template_part("/template-parts/account-edit-account"); ?>
-
-
 </section>
