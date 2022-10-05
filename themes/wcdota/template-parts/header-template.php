270e0b8d8776016a7c31779dd71b4dcea5c66230
EddieWebDev <eddandersson@gmail.com>
2022-10-05 21:35:01 +0200

    fixed header logo link size

--- themes/wcdota/template-parts/header-template.php
+++ themes/wcdota/template-parts/header-template.php
@@ -1,9 +1,11 @@
 <nav id="header-nav">
     <?php wp_nav_menu(array('theme_location' => 'header-menu')); ?>
     <div class="header-title">
-        <h1 class="site-name">
-            <?php echo get_bloginfo('name'); ?>
-        </h1>
+        <a class="site-name-link" href="/wcdota/">
+            <h1 class="site-name">
+                <?php echo get_bloginfo('name'); ?>
+            </h1>
+        </a>
     </div>
     
     <div class="header-login">
